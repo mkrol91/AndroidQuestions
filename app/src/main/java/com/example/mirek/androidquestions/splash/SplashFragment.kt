aede78a85fbb385d9b39e3f96d8f9b2975e22595
Mirosław Król <mkrol@concisesoftware.com>
2018-05-07 15:00:43 +0200

    Anim in DSL (almost)

--- app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
+++ app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
@@ -1,16 +1,21 @@
 package com.example.mirek.androidquestions.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.constraint.ConstraintSet
 import android.support.v4.app.Fragment
+import android.transition.ChangeBounds
+import android.transition.Transition
 import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
 import com.example.mirek.androidquestions.R
+import com.example.mirek.androidquestions.util.emptyUnit
 import kotlinx.android.synthetic.main.splash_fragment_initial.*
 
-class SplashFragment : Fragment() {
+class SplashFragment() : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.splash_fragment_initial, container, false)
@@ -22,21 +27,61 @@ class SplashFragment : Fragment() {
         val initialConstraints = ConstraintSet()
         initialConstraints.clone(splash_root)
 
-        val finalConstraints = ConstraintSet()
-        finalConstraints.clone(activity, R.layout.splash_fragment_second)
+        val secondConstraints = ConstraintSet()
+        secondConstraints.clone(activity, R.layout.splash_fragment_second)
 
-        toggleFirstAndSecondSplashState(initialConstraints, finalConstraints)
+        val thirdConstraints = ConstraintSet()
+        thirdConstraints.clone(activity, R.layout.splash_fragment_third)
+
+        val transition = customTransition({
+            interpolator = AccelerateInterpolator()
+            duration = 3000
+        }, {
+            applyAnimationToRoot(thirdConstraints, this)
+        })
+
+        Handler().postDelayed({
+            applyAnimationToRoot(secondConstraints, transition)
+        }, 3000)
     }
 
-    private fun toggleFirstAndSecondSplashState(initialConstraints: ConstraintSet, finalConstraints: ConstraintSet) {
-        var set = false
-        floatingActionButton1.setOnClickListener {
-            TransitionManager.beginDelayedTransition(splash_root)
-            val constraint = if (set) initialConstraints else finalConstraints
-            constraint.applyTo(splash_root)
-            set = !set
-        }
+    fun customTransition(block: ChangeBounds.() -> Unit, endAction: ChangeBounds.() -> Unit): ChangeBounds {
+        val changeBounds = ChangeBounds()
+        changeBounds.block()
+        changeBounds.addListener(object : Transition.TransitionListener {
+            override fun onTransitionEnd(transition: Transition?) {
+                changeBounds.endAction()
+            }
+
+            override fun onTransitionResume(transition: Transition?) {
+            }
+
+            override fun onTransitionPause(transition: Transition?) {
+            }
+
+            override fun onTransitionCancel(transition: Transition?) {
+            }
+
+            override fun onTransitionStart(transition: Transition?) {
+            }
+        })
+        return changeBounds
     }
+
+    private fun applyAnimationToRoot(newConstraints: ConstraintSet, preparedTransition: ChangeBounds, transitionEndAction: () -> Unit = emptyUnit()) {
+        TransitionManager.beginDelayedTransition(splash_root, preparedTransition)
+        newConstraints.applyTo(splash_root)
+    }
+
+//    Transition explode = new Explode()
+//    .setEpicenterCallback(new Transition.EpicenterCallback() {
+//        @Override
+//        public Rect onGetEpicenter(Transition transition) {
+//            return viewRect;
+//        }
+//    });
+//    explode.setDuration(1000);
+//    TransitionManager.beginDelayedTransition(recyclerView, explode);
 
     companion object {
         fun newInstance(): SplashFragment {
@@ -44,3 +89,4 @@ class SplashFragment : Fragment() {
         }
     }
 }
+
---
+++ app/src/main/java/com/example/mirek/androidquestions/util/GeneralUtils.kt
@@ -0,0 +1,3 @@
+package com.example.mirek.androidquestions.util
+
+fun emptyUnit(): () -> Unit = {}
