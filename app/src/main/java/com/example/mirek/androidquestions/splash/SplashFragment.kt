173879e7ae190f7b148196dac50080ef65489725
Mirosław Król <mkrol@concisesoftware.com>
2018-05-08 08:54:15 +0200

    Fly away transition

--- app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
+++ app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
@@ -1,18 +1,20 @@
 package com.example.mirek.androidquestions.splash
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.support.constraint.ConstraintSet
+import android.support.transition.ChangeBounds
+import android.support.transition.Explode
+import android.support.transition.Transition
+import android.support.transition.TransitionManager
 import android.support.v4.app.Fragment
-import android.transition.ChangeBounds
-import android.transition.Transition
-import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
+import android.view.animation.AnticipateOvershootInterpolator
 import com.example.mirek.androidquestions.R
-import com.example.mirek.androidquestions.util.emptyUnit
 import kotlinx.android.synthetic.main.splash_fragment_initial.*
 
 class SplashFragment() : Fragment() {
@@ -27,68 +29,73 @@ class SplashFragment() : Fragment() {
         val initialConstraints = ConstraintSet()
         initialConstraints.clone(splash_root)
 
-        val secondTransition = customTransition({
+        val secondChangeBounds = customChangeBoundsTransition({
             interpolator = AccelerateInterpolator()
-            duration = 3000
-        }, {})
+            duration = 1000
+        }, {
+            val explode = ChangeBounds()
+            explode.duration = 500
+            explode.interpolator = AccelerateInterpolator()
 
-        val firstTransition = customTransition(
+            val fourthConstraints = ConstraintSet()
+            fourthConstraints.clone(activity, R.layout.splash_fragment_fourth)
+            applyAnimationToRoot(fourthConstraints, explode)
+        })
+
+        val firstChangeBounds = customChangeBoundsTransition(
                 {
                     interpolator = AccelerateInterpolator()
-                    duration = 3000
+                    duration = 1000
                 },
                 {
                     val thirdConstraints = ConstraintSet()
                     thirdConstraints.clone(activity, R.layout.splash_fragment_third)
-                    applyAnimationToRoot(thirdConstraints, secondTransition)
+                    applyAnimationToRoot(thirdConstraints, secondChangeBounds)
                 }
         )
 
         Handler().postDelayed({
             val secondConstraints = ConstraintSet()
             secondConstraints.clone(activity, R.layout.splash_fragment_second)
-            applyAnimationToRoot(secondConstraints, firstTransition)
-        }, 3000)
+            applyAnimationToRoot(secondConstraints, firstChangeBounds)
+        }, 1000)
 
     }
 
-    fun customTransition(block: ChangeBounds.() -> Unit, endAction: ChangeBounds.() -> Unit): ChangeBounds {
-        val changeBounds = ChangeBounds()
-        changeBounds.block()
-        changeBounds.addListener(object : Transition.TransitionListener {
-            override fun onTransitionEnd(transition: Transition?) {
-                changeBounds.endAction()
-            }
+    fun customChangeBoundsTransition(initParameters: ChangeBounds.() -> Unit, onTransitionEnd: ChangeBounds.() -> Unit): ChangeBounds {
+        return ChangeBounds().apply {
+            initParameters()
+            addListener(object : Transition.TransitionListener {
+                override fun onTransitionEnd(transition: Transition) {
+                    onTransitionEnd()
+                }
 
-            override fun onTransitionResume(transition: Transition?) {
-            }
+                override fun onTransitionResume(transition: Transition) {
+                }
 
-            override fun onTransitionPause(transition: Transition?) {
-            }
+                override fun onTransitionPause(transition: Transition) {
+                }
 
-            override fun onTransitionCancel(transition: Transition?) {
-            }
+                override fun onTransitionCancel(transition: Transition) {
+                }
 
-            override fun onTransitionStart(transition: Transition?) {
-            }
-        })
-        return changeBounds
+                override fun onTransitionStart(transition: Transition) {
+                }
+            })
+        }
     }
+
 
     private fun applyAnimationToRoot(newConstraints: ConstraintSet, preparedTransition: ChangeBounds) {
         TransitionManager.beginDelayedTransition(splash_root, preparedTransition)
         newConstraints.applyTo(splash_root)
     }
 
-//    Transition explode = new Explode()
-//    .setEpicenterCallback(new Transition.EpicenterCallback() {
-//        @Override
-//        public Rect onGetEpicenter(Transition transition) {
-//            return viewRect;
-//        }
-//    });
-//    explode.setDuration(1000);
-//    TransitionManager.beginDelayedTransition(recyclerView, explode);
+    private fun applyAnimationToRoot(newConstraints: ConstraintSet, preparedTransition: Explode) {
+        TransitionManager.beginDelayedTransition(splash_root, preparedTransition)
+        newConstraints.applyTo(splash_root)
+    }
+
 
     companion object {
         fun newInstance(): SplashFragment {
