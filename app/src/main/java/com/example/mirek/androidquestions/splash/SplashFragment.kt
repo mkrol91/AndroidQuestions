d3c6ddc9093e74a5cfe4ad4408c31869401d33bb
Mirosław Król <mkrol@concisesoftware.com>
2018-05-09 13:16:42 +0200

    SplashFragment anims as Completables

--- app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
+++ app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
@@ -2,21 +2,21 @@ package com.example.mirek.androidquestions.splash
 
 import android.animation.TimeInterpolator
 import android.os.Bundle
+import android.os.Handler
 import android.support.constraint.ConstraintSet
 import android.support.transition.ChangeBounds
 import android.support.transition.Transition
 import android.support.transition.TransitionManager
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import com.example.mirek.androidquestions.R
-import io.reactivex.Observable
+import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.splash_fragment_initial.*
-import java.util.concurrent.TimeUnit
 
 class SplashFragment() : Fragment() {
 
@@ -27,50 +27,102 @@ class SplashFragment() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val animationPhases = arrayListOf(
-                ConstraintSet().apply {
-                    clone(splash_root)
-                },
-                ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_second)
-                },
-                ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_third)
-                },
-                ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_fourth)
-                },
-                ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_f)
-                }
-        )
+//        val sixthTransition = customChangeBoundsTransition(500, AccelerateInterpolator(), {})
+//        val fifthTransition = customChangeBoundsTransition(5000, AccelerateInterpolator(),
+//                {
+//                    //  applyAnimationToRoot(animationPhases[4], sixthChangeBounds)
+//                })
+//        val fourthTransition = customChangeBoundsTransition(1000, AccelerateInterpolator(),
+//                {
+//                    applyAnimationToRoot(animationPhases[3], fifthTransition)
+//                })
+//        val thirdTransition = customChangeBoundsTransition(1000, AccelerateInterpolator(),
+//                {
+//                    applyAnimationToRoot(animationPhases[2], fourthTransition)
+//                })
+//        val secondTransition = customChangeBoundsTransition(1000, AccelerateInterpolator(),
+//                {
+//                    applyAnimationToRoot(animationPhases[1], thirdTransition)
+//                })
 
+//        val animationPhases = arrayListOf(
+//                AnimationPhase(
+//                        ConstraintSet().apply {
+//                            clone(splash_root)
+//                        }, 1000, AccelerateInterpolator()),
+//                AnimationPhase(
+//                        ConstraintSet().apply {
+//                            clone(activity, R.layout.splash_fragment_second)
+//                        }, 1000, AccelerateInterpolator()),
+//                AnimationPhase(
+//                        ConstraintSet().apply {
+//                            clone(activity, R.layout.splash_fragment_third)
+//                        }, 1000, AccelerateInterpolator()),
+//                AnimationPhase(
+//                        ConstraintSet().apply {
+//                            clone(activity, R.layout.splash_fragment_fourth)
+//                        }, 1000, AccelerateInterpolator()),
+//                AnimationPhase(
+//                        ConstraintSet().apply {
+//                            clone(activity, R.layout.splash_fragment_f)
+//                        }, 5000, AccelerateInterpolator())
+//        )
 
-        val sixthChangeBounds = customChangeBoundsTransition(500, AccelerateInterpolator(), {})
-        val fifthChangeBounds = customChangeBoundsTransition(5000, AccelerateInterpolator(),
-                {
-                    //  applyAnimationToRoot(animationPhases[4], sixthChangeBounds)
-                })
-        val fourthChangeBounds = customChangeBoundsTransition(1000, AccelerateInterpolator(),
-                {
-                    applyAnimationToRoot(animationPhases[3], fifthChangeBounds)
-                })
-        val thiChangeBounds = customChangeBoundsTransition(1000, AccelerateInterpolator(),
-                {
-                    applyAnimationToRoot(animationPhases[2], fourthChangeBounds)
-                })
-        val secChangeBounds = customChangeBoundsTransition(1000, AccelerateInterpolator(),
-                {
-                    applyAnimationToRoot(animationPhases[1], thiChangeBounds)
-                })
-
-        Observable.just(1).subscribeOn(Schedulers.io())
-                .delay(3000, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    applyAnimationToRoot(animationPhases[0], secChangeBounds)
-                }
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        Handler().postDelayed(Runnable {
+            val animation1 = Completable.create {
+                applyAnimationToRoot(ConstraintSet().apply {
+                    clone(activity, R.layout.splash_fragment_second)
+                }, customChangeBoundsTransition(1000, AccelerateInterpolator(), {
+                    Log.i("animationFlow", "anim 1 finished")
+                    it.onComplete()
+                }))
+            }
+
+            val animation2 = Completable.create {
+                applyAnimationToRoot(ConstraintSet().apply {
+                    clone(activity, R.layout.splash_fragment_third)
+                }, customChangeBoundsTransition(1000, AccelerateInterpolator(), {
+                    Log.i("animationFlow", "anim 2 finished")
+                    it.onComplete()
+                }))
+            }
+
+            val animation3 = Completable.create {
+                applyAnimationToRoot(ConstraintSet().apply {
+                    clone(activity, R.layout.splash_fragment_fourth)
+                }, customChangeBoundsTransition(1000, AccelerateInterpolator(), {
+                    Log.i("animationFlow", "anim 3 finished")
+                    it.onComplete()
+                }))
+            }
+
+            val animation4 = Completable.create {
+                explosion.animate().alpha(0f).withEndAction {
+                    it.onComplete()
+                    Log.i("animationFlow", "anim 4 finished")
+                }
+            }
+
+            animation1.andThen(animation2).andThen(animation3).andThen(animation4)
+                    .subscribeOn(AndroidSchedulers.mainThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        Log.i("abcd", "success")
+                    }, {
+                        Log.i("abcd", "error:" + it)
+                    })
+
+        }, 10)
+    }
+
+    class AnimationPhase(var constraintSet: ConstraintSet,
+                         var durationTime: Long,
+                         var interpolator: TimeInterpolator)
 
     fun customChangeBoundsTransition(durationTime: Long, interpolatorType: TimeInterpolator,
                                      onTransitionEnd: ChangeBounds.() -> Unit): ChangeBounds {
@@ -79,19 +131,24 @@ class SplashFragment() : Fragment() {
             interpolator = interpolatorType
             addListener(object : Transition.TransitionListener {
                 override fun onTransitionEnd(transition: Transition) {
+                    Log.i("abcd", "trans end")
                     onTransitionEnd()
                 }
 
                 override fun onTransitionResume(transition: Transition) {
+                    Log.i("abcd", "trans resume")
                 }
 
                 override fun onTransitionPause(transition: Transition) {
+                    Log.i("abcd", "trans pause")
                 }
 
                 override fun onTransitionCancel(transition: Transition) {
+                    Log.i("abcd", "trans cancel")
                 }
 
                 override fun onTransitionStart(transition: Transition) {
+                    Log.i("abcd", "trans start")
                 }
             })
         }
