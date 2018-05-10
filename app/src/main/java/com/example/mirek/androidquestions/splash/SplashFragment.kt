07b733ffb9fffc8ffbf95c715b904e8ef017d614
Mirosław Król <mkrol@concisesoftware.com>
2018-05-10 08:34:56 +0200

    Animations using concat

--- app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
+++ app/src/main/java/com/example/mirek/androidquestions/splash/SplashFragment.kt
@@ -16,6 +16,7 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.OvershootInterpolator
 import com.example.mirek.androidquestions.R
 import io.reactivex.Completable
+import io.reactivex.CompletableEmitter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.splash_fragment_initial.*
 
@@ -32,81 +33,83 @@ class SplashFragment() : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        Handler().postDelayed(Runnable {
-            val animation1 = Completable.create {
-                applyAnimationToRoot(ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_second)
-                }, customChangeBoundsTransition(500, AccelerateInterpolator(), {
-                    Log.i("animationFlow", "anim 1 finished")
-                    it.onComplete()
-                }))
-            }
+        var animations = arrayListOf(
+                Completable.create {
+                    //TODO: How to do it better or how to remove it?
+                    Handler().postDelayed({
+                        it.onComplete()
+                    }, 100)
+                },
+                Completable.create {
+                    applyAnimationToRoot(ConstraintSet().apply {
+                        clone(activity, R.layout.splash_fragment_second)
+                    }, changeBoundsTransition(500, AccelerateInterpolator(), it))
+                },
+                Completable.create {
+                    applyAnimationToRoot(ConstraintSet().apply {
+                        clone(activity, R.layout.splash_fragment_third)
+                    }, changeBoundsTransition(2000, AccelerateInterpolator(), it))
+                },
+                Completable.create {
+                    applyAnimationToRoot(ConstraintSet().apply {
+                        clone(activity, R.layout.splash_fragment_fourth)
+                    }, changeBoundsTransition(200, OvershootInterpolator(), it))
+                },
+                Completable.create {
+                    explosion.animate().alpha(0f)
+                            .withEndAction { it.onComplete() }.duration = 200
+                },
+                Completable.create {
+                    applyAnimationToRoot(ConstraintSet().apply {
+                        clone(activity, R.layout.splash_fragment_end)
+                    }, changeBoundsTransition(300, OvershootInterpolator(), it))
+                },
+                Completable.create {
+                    atCs.visibility = View.VISIBLE
+                    atCs.alpha = 0f
+                    atCs.animate().alpha(1f).withEndAction { it.onComplete() }.duration = 5000
+                }
+        )
 
-            val animation2 = Completable.create {
-                applyAnimationToRoot(ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_third)
-                }, customChangeBoundsTransition(2000, AccelerateInterpolator(), {
-                    Log.i("animationFlow", "anim 2 finished")
-                    it.onComplete()
-                }))
-            }
-
-            val animation3 = Completable.create {
-                applyAnimationToRoot(ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_fourth)
-                }, customChangeBoundsTransition(200, OvershootInterpolator(), {
-                    Log.i("animationFlow", "anim 3 finished")
-                    it.onComplete()
-                }))
-            }
-
-            val animation4 = Completable.create {
-                explosion.animate().alpha(0f).withEndAction {
-                    it.onComplete()
-                    Log.i("animationFlow", "anim 4 finished")
-                }.duration = 200
-            }
-
-            val animation5 = Completable.create {
-                applyAnimationToRoot(ConstraintSet().apply {
-                    clone(activity, R.layout.splash_fragment_end)
-                }, customChangeBoundsTransition(300, OvershootInterpolator(), {
-                    Log.i("animationFlow", "anim 5 finished")
-                    it.onComplete()
-                }))
-            }
-
-            val animation6 = Completable.create {
-                atCs.visibility = View.VISIBLE
-                atCs.alpha = 0f
-                atCs.animate().alpha(1f).withEndAction {
-                    it.onComplete()
-                    Log.i("animationFlow", "anim 5 finished")
-                }.duration = 5000
-            }
-
-            animation1.andThen(animation2).andThen(animation3).andThen(animation4).andThen(animation5)
-                    .andThen(animation6)
-                    .subscribeOn(AndroidSchedulers.mainThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({
-                        Log.i("abcd", "success")
-                    }, {
-                        Log.i("abcd", "error:" + it)
-                    })
-
-        }, 10)
+        Completable.concat(animations)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Log.i("abcd", "success")
+                }, {
+                    Log.i("abcd", "error:" + it)
+                })
+        //Other way to do this:
+//        Completable.create {
+//            //TODO: How to do it better or how to remove it?
+//            Handler().postDelayed({
+//                it.onComplete()
+//            }, 100)
+//        }
+//                .andThen(animation1)
+//                .andThen(animation2)
+//                .andThen(animation3)
+//                .andThen(animation4)
+//                .andThen(animation5)
+//                .andThen(animation6)
+//                .subscribeOn(AndroidSchedulers.mainThread())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe({
+//                    Log.i("abcd", "success")
+//                }, {
+//                    Log.i("abcd", "error:" + it)
+//                })
     }
 
-    fun customChangeBoundsTransition(durationTime: Long, interpolatorType: TimeInterpolator,
-                                     onTransitionEnd: ChangeBounds.() -> Unit): ChangeBounds {
+    fun changeBoundsTransition(durationTime: Long, interpolatorType: TimeInterpolator,
+                               emitter: CompletableEmitter): ChangeBounds {
         return ChangeBounds().apply {
             duration = durationTime
             interpolator = interpolatorType
             addListener(object : Transition.TransitionListener {
                 override fun onTransitionEnd(transition: Transition) {
                     Log.i("abcd", "trans end")
-                    onTransitionEnd()
+                    emitter.onComplete()
                 }
 
                 override fun onTransitionResume(transition: Transition) {
